07348ed4a163690a024052d2f86e6a08ad93af4e
Aaron Daniel <aaron.daniel@iinerds.com>
2019-10-10 10:59:17 +0530

    cleared all

--- Lambda/index.js
+++
@@ -1,9 +0,0 @@
-var time = require('time');
-exports.GreetingLambda = (event, context, callback) => {
-	    var currentTime = new time.Date(); 
-	    currentTime.setTimezone("America/Los_Angeles");
-	    callback(null, {
-		            statusCode: '200',
-		            body: 'The Time In Texas is: ' + currentTime.toString(),
-		        });
-};
