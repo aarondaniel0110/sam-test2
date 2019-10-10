f01ddac7d6f12e0f382482c127d7466cf7dde756
aarondaniel0110 <35559885+aarondaniel0110@users.noreply.github.com>
2019-10-10 11:13:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello world..This is a test node web-service running on render.\n');
 });
 
 app.listen(PORT, HOST);
